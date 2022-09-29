f08abda611a9b845b789236098ef235594910a95
Yongjia Sun <thomastank0926@gmail.com>
2022-09-29 04:33:58 +0000

    modularize the input from keyboard

---
+++ input.js
@@ -0,0 +1,24 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // creat an event listener for stdin
+  // runs handleUserInput when receiving input from keyboard
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// callback function for stdin.on("data", handleUserInput)
+const handleUserInput = function(key) {
+  // terminate the game when ctrl + C is inputed
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {setupInput};
